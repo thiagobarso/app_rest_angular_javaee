9cf27682cad87b09f5d61ab2ec77e768872773a4
Thiago Barbosa <thiagobarso@gmail.com>
2016-09-04 18:36:03 -0300

    parte 5 - dando erro 415 (Unsupported Media Type)

--- WebContent/control/produto.js
+++ WebContent/control/produto.js
@@ -1,36 +1,74 @@
 var produtoModule = angular.module('produtoModule',[]);
 
-produtoModule.controller("produtoControl",function($scope){
+produtoModule.controller("produtoControl",function($scope,$http){
 	
-	console.log('Fornecedores');
-	$scope.fornecedores = [
-		{codigo:1, nome: 'Alpargatas'},
-		{codigo:2, nome: 'Sádia'},
-		{codigo:3, nome: 'Nestle'}
-	];
+	urlFornecedor = 'http://localhost:8080/SCP/rs/fornecedor';	
 
-	console.log('Produtos');
-	$scope.produtos = [
-		{codigo:1, nome: 'Sandalia Havaiana', fornecedor: {codigo:1, nome: 'Alpargatas'}},
-		{codigo:2, nome: 'Bombom Nestle', fornecedor: {codigo:3, nome: 'Nestle'}}		
-	];
+	urlProduto = 'http://localhost:8080/SCP/rs/produto';	
+	
+	$scope.pesquisarFornecedor = function(){
+		console.log('pesquisando fornecedores');
+		$http.get(urlFornecedor).success(function (fornecedores){
+			$scope.fornecedores = fornecedores;
+		}).error(function(erro){
+			alert(erro);
+		});
+	}
+	
+	$scope.pesquisarProduto = function(){
+		console.log('pesquisando produto');
+		$http.get(urlProduto).success(function (produtos){
+			$scope.produtos = produtos;
+		}).error(function(erro){
+			alert(erro);
+		});
+	}	
+
+	$scope.salvar = function(){
+		if($scope.produto.codigo == ''){
+			$http.post(urlProduto,$scope.produto).success(function(produto){
+				$scope.produtos.push($scope.produto);
+				$scope.novo();
+			}).error(function(erro){
+				alert(erro);
+			});
+		}else{
+			$http.put(urlProduto,$scope.produto).success(function(produto){
+				$scope.pesquisarProduto();
+				$scope.novo();
+			}).error(function(erro){
+				alert(erro);
+			});
+		}			
+	}
+	
+	$scope.excluir = function(){
+		if($scope.produto.codigo == ''){
+			alert('Selecione um produto');
+		}else{
+			var urlExcluir = urlProduto + "/" + $scope.produto.codigo;
+			$http.delete(urlExcluir).success(function(){
+				$scope.pesquisarProduto();
+				$scope.novo();
+			}).error(function(erro){
+				alert(erro);
+			});
+			
+		}
+		$scope.produtos.splice($scope.produtos.indexOf($scope.produto),1);
+		$scope.novo();
+	}
 
 	$scope.selecionaProduto = function(produto){
 		$scope.produto = produto;		
 	}
 
-	$scope.salvar = function(){
-		$scope.produtos.push($scope.produto);
-		$scope.novo();
-	}
-
-	$scope.excluir = function(){
-		$scope.produtos.splice($scope.produtos.indexOf($scope.produto),1);
-		$scope.novo();
-	}
 
 	$scope.novo = function(){
 		$scope.produto = "";
 	}
+	
+	$scope.pesquisarProduto();
+	$scope.pesquisarFornecedor();
 
 });
